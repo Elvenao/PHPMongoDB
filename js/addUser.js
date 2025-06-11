04fd3c2e64466bbafd60aa19244d1d90a6d39b09
Elvenao <emhdzss@gmail.com>
2025-06-11 17:54:47 -0600

    change in image presentation

--- js/addUser.js
+++ js/addUser.js
@@ -25,7 +25,7 @@ function addUser(){
 
 function deleteUser(id){
     let colection = "Users"
-    let ruta = "<?php echo SITE_URL;?>"
+    let ruta = "<?php echo SITE_URL;?>Agregar/Usuarios"
     let datos = JSON.stringify({colection,id})
     llamadaASweetAlert(datos,"<?php echo SITE_URL;?>_controller/deleteUserAjax.php","Eliminar Usuario","Eliminar Usuario","question","Eliminar","Cancelar","No fue posible eliminar el usuario","Se eliminar con exito","El usuario fue eliminado con exito",true,ruta)
 }
